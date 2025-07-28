dd116cd4ed9933ac822228b0729f36d9bc7fbb01
Sumit Alam Khan <31077557+Tsumiiiiiiii@users.noreply.github.com>
2025-07-29 00:17:39 +0600

    Update sol.py

--- Deadsec CTF 25/imrul kAES/sol.py
+++ Deadsec CTF 25/imrul kAES/sol.py
@@ -1,22 +1,122 @@
-from tqdm import tqdm
+import socket
 
-n=144984891276196734965453594256209014778963203195049670355310962211566848427398797530783430323749867255090629853380209396636638745366963860490911853783867871911069083374020499249275237733775351499948258100804272648855792462742236340233585752087494417128391287812954224836118997290379527266500377253541233541409
-c=120266872496180344790010286239079096230140095285248849852750641721628852518691698502144313546787272303406150072162647947041382841125823152331376276591975923978272581846998438986804573581487790011219372437422499974314459242841101560412534631063203123729213333507900106440128936135803619578547409588712629485231
-hint=867001369103284883200353678854849752814597815663813166812753132472401652940053476516493313874282097709359168310718974981469532463276979975446490353988
+def sendline(sock, data):
+    sock.sendall(data + b"\n")
 
-P.<x> = PolynomialRing(ZZ)
+def recvline(sock):
+    buf = b""
+    while True:
+        data = sock.recv(1)
+        if not data:
+            break
+        buf += data
+        if data == b"\n":
+            break
+    return buf
 
-phi_up = n >> (1024 - 505)
-phi_lo = hint
-e = 65537
+def recvuntil(sock, delim):
+    buf = b""
+    while True:
+        data = sock.recv(1)
+        if not data:
+            break
+        buf += data
+        if delim in buf:
+            break
+    return buf
 
-for mask in tqdm(range(1 << 19)):
-    phi_ = (phi_up << (1024-505)) + (mask << hint.bit_length()) + phi_lo
-    f = x^2 + x*(phi_ - n - 1) + n
-    for p_, _ in f.roots():
-        if n % int(p_) == 0:
-            print(p_)
-            p, q = p_, n // p_
-            d = pow(e, -1, (p-1)*(q-1))
-            msg = pow(c, int(d), n)
-            print(int(msg).to_bytes(1024, 'big').lstrip(b'\x00'))
+from Crypto.Util.number import long_to_bytes, bytes_to_long
+
+def conn():
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.connect(('34.44.186.163', 31510))
+  return sock
+
+io = conn()
+
+def query(io, i):
+    recvuntil(io, b': ')
+    sendline(io, str(i).encode())
+    r = recvline(io).decode().strip()
+    if r.startswith('bad'):
+        return -1
+    else:
+        return bytes.fromhex(r)
+
+def batch_query(io, batch):
+    to_send = '\n'.join(str(i) for i in batch)
+    sendline(io, to_send.encode())
+    
+    ret = []
+    for _ in range(len(batch)):
+        r = bytes.fromhex(recvline(io).decode().strip().split(': ')[-1])
+        ret.append(r)
+    return ret
+
+def get_blocks(s):
+    ret = []
+    for i in range(0, len(s), 16):
+        ret.append(s[i : i + 16])
+    return ret
+
+def get_modulus(io, verbose = False):
+    lo, hi = 1, 1 << 1024
+    n = -1
+    cnt = 1
+    while lo <= hi:
+        if verbose and cnt % 100 == 0:
+            print(f'query no {cnt}')
+        cnt += 1
+        mid = (lo + hi) // 2
+        if query(io, mid) == -1:
+            n = mid
+            hi = mid - 1
+        else:
+            lo = mid + 1
+            
+    for n in range(n - 2, n + 1):
+        if query(io, n) == -1:
+            return n
+    return -1
+            
+
+print(recvline(io))
+
+ct = int(recvline(io).decode().strip())
+n = get_modulus(io, verbose = True)
+print(f'the modulus is {n}')
+e = 12389231641983877009741841713701317189420787527171545487350619433744301520682298136425919859970313849150196317044388637723151690904279767516595936892361663
+
+
+sz = 34
+
+sofar = b''
+
+for i in range(63, 0, -1):
+    to_send = ct * pow(256, i * e, n)
+    to_send %= n
+    blocks = get_blocks(query(io, to_send))
+    batch = []
+    for c in range(32, 128):
+        if len(sofar) > 15:
+            take = sofar[:15]
+        else:
+            take = sofar
+        ts = bytes([c]) + take
+        while len(ts) != 16: ts += b'\x00'
+        batch.append(pow(bytes_to_long(ts), e, n))
+
+    r = batch_query(io, batch)
+    found = None
+    for c, v in zip(range(32, 128), r):
+        if blocks[0] == get_blocks(v)[0]:
+            found = c
+            break
+            
+    if not found:
+        print('everything all g?')
+        break
+    sofar = bytes([found]) + sofar
+    print(sofar)
+    if b'{' in sofar:
+        break
